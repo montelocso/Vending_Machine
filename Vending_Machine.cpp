c53e9fd3172e1616f16001ad2c71ffc219800e3f
Guilherme Montelo <montelocso@gmail.com>
2023-06-12 03:32:46 -0300

    Primeira versão 100% Funcional

--- Vending_Machine.cpp
+++ Vending_Machine.cpp
@@ -15,7 +15,9 @@ struct stProdutos {
 
 stProdutos Espaco[5];
 int iContador;
+float fTotalVendas;
 
+/*Função que permite fazer a carga inicial das mercadorias e repor/alterar itens*/
 void fAltMercadoria() {
     int iOpcao;
     void administrador();
@@ -31,8 +33,9 @@ void fAltMercadoria() {
         cin >> iOpcao;
         if (iOpcao >= 0 && iOpcao <= 4) {
             cout << setw(10) << left << Espaco[iOpcao].iCodProduto << setw(15) << left << Espaco[iOpcao].sNomeProduto << setw(15) << left << Espaco[iOpcao].fValor << setw(15) << left << Espaco[iOpcao].iEstoque << endl;
-            cout << "Digite o novo codigo do produto: " << endl;
-            cin >> Espaco[iOpcao].iCodProduto;
+            //cout << "Digite o novo codigo do produto: " << endl;
+            //cin >> Espaco[iOpcao].iCodProduto;
+            Espaco[iOpcao].iCodProduto = iOpcao + 1;
             cout << "Digite o novo nome do produto: " << endl;
             cin >> Espaco[iOpcao].sNomeProduto;
             cout << "Digite o novo valor do produto: " << endl;
@@ -54,6 +57,7 @@ void fAltMercadoria() {
     } while (iOpcao >= 0 && iOpcao <= 4);
 }
 
+/*Função que lista o estoque atual da máquina*/
 void fInventario() {
     int iOpcao;
     void administrador();
@@ -79,11 +83,46 @@ void fInventario() {
     } while (iOpcao != 1 && iOpcao != 2);
 }
 
-void fFaturamento(){
+/*Função que permite contabilizar o lucro real e o lucro possivel*/
+void fFaturamento() {
+    void administrador();
+    int iOpcao;
+    float fValorTotal = 0;
+    cout << "1 - Saldo de Vendas\n2 - Potencial de Vendas\n3 - Voltar\n4 - Sair" << endl;
+    cin >> iOpcao;
+    switch (iOpcao) {
+        case 1:
+            cout << "Saldo de Vendas: R$ " << fTotalVendas << endl;
+        break;
+        case 2:
+            cout << setw(40) << "Estoque atual" << endl;
+            cout << "---------------------------------------------------------------" << endl;
+            cout << setw(10) << left << "Codigo" << setw(15) << left << "Mercadoria" << setw(15) << left << "Preço" << setw(15) << left << "Quantidade" << setw(10) << left << "Potencial Lucro" << endl;
+    
+            for (iContador = 0; iContador < 5 ; iContador++) {
+                cout << setw(10) << left << Espaco[iContador].iCodProduto << setw(15) << left << Espaco[iContador].sNomeProduto << setw(15) << left << Espaco[iContador].fValor << setw(15) << left << Espaco[iContador].iEstoque << setw(10) << left << Espaco[iContador].iEstoque * Espaco[iContador].fValor << endl;  
+                fValorTotal = fValorTotal + (Espaco[iContador].iEstoque * Espaco[iContador].fValor);
+            }
+            cout << setw(56) << right << fValorTotal << endl;
+            administrador();
+        break;
+        case 3:
+            administrador();
+        break;
+        case 4:
+            cout << "Sair" << endl;
+        break;
+        default:
+            cout << "Opcao invalida" << endl;
+            fFaturamento();
+        break;
+    }
 
 }
 
+/*Modulo de administrador*/
 void administrador() {
+    void fCliente();
     void fInventario();
     cout << setw(40) << "Modulo Administrador" << endl;
     cout << "--------------------------------------------------" << endl;
@@ -109,7 +148,7 @@ void administrador() {
             break;
         case 4:
             cout << "Sair" << endl;
-                main();
+                fCliente();
             break;
         default:
             cout << "Opcao invalida" << endl;
@@ -117,10 +156,51 @@ void administrador() {
     }
 }
 
-void cliente() {
-
+/*Modulo de vendas/clientes*/
+void fCliente() {
+    int iOpcao;
+    float fValorInserido;
+    do {
+        cout << setw(40) << "Maquina de Venda Automatica" << endl;
+        cout << "--------------------------------------------------" << endl;
+        cout << setw(10) << left << "Codigo" << setw(15) << left << "Mercadoria" << setw(15) << left << "Preço" << setw(15) << left << "Quantidade" << endl;
+    
+        for (iContador = 0; iContador < 5 ; iContador++) {
+            cout << setw(10) << left << Espaco[iContador].iCodProduto << setw(15) << left << Espaco[iContador].sNomeProduto << setw(15) << left << Espaco[iContador].fValor << setw(15) << left << Espaco[iContador].iEstoque << endl;   
+        }
+        cout << "Digite o codigo do produto que deseja comprar: ";
+        cin >> iOpcao;
+        iOpcao = iOpcao - 1;
+        if (Espaco[iOpcao].iEstoque > 0) {
+            cout << "Digite o valor inserido na máquina" << endl;
+            cin >> fValorInserido;
+            if (fValorInserido >= Espaco[iOpcao].fValor) {
+                fTotalVendas = fTotalVendas + Espaco[iOpcao].fValor;
+                Espaco[iOpcao].iEstoque = Espaco[iOpcao].iEstoque - 1;
+                cout << "Compra efetuada com sucesso" << endl;
+                cout << "Troco: " << fValorInserido - Espaco[iOpcao].fValor << endl;
+            }
+            else {
+                cout << "Valor insuficiente" << endl;
+            }
+        }
+        else {
+            cout << "Produto indisponivel" << endl;
+            cout << "Deseja acessar o modulo administrador?" << endl;
+            cout << "1 - Sim\n2 - Nao" << endl;
+            cin >> iOpcao;
+            if (iOpcao == 1) {
+                administrador();
+            }
+            else {
+                cout << "Voltar" << endl;
+                fCliente();
+            }
+        }
+    } while (iOpcao >= 0 && iOpcao <= 4);
 }
 
+/*Modulo de inicialização que demostra o estoque inicial vazio e da acesso ao modulo de administrador*/
 int main() {
     cout << setw(40) << "Maquina de Venda Automatica" << endl;
     cout << "--------------------------------------------------" << endl;
@@ -132,5 +212,5 @@ int main() {
     if (Espaco[0].iEstoque + Espaco[1].iEstoque + Espaco[2].iEstoque + Espaco[3].iEstoque + Espaco[4].iEstoque == 0) {
         administrador();
     }
-    
+    return 0;  
 }
